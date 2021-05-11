b208275e9a764f0aab48be4bfd81c994558ae9b7
Casper <casper.iversen2@icloud.com>
2021-05-11 07:25:24 +0200

    add docker to install.sg

--- install.sh
+++ install.sh
@@ -68,3 +68,25 @@ echo "\nINFO: Installed correct .zshrc\n"
 # install github cli
 
 brew install gh
+
+echo "\nInstalled github cli and Homebrew\n"
+
+# docker install
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+sudo apt-get update
+
+ sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+   
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+ 
+ sudo apt-get update
+
+ sudo apt-get install docker-ce docker-ce-cli containerd.io
+ 
+ echo "\nInstalled Docker\n"
