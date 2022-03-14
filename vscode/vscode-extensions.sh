b0a9e7332017ac604ac7feffd05eae191b241465
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-03-14 17:36:17 +0100

    :hammer: chore: update configs

--- vscode/vscode-extensions.sh
+++ vscode/vscode-extensions.sh
@@ -10,31 +10,32 @@ PACKAGES=(
    christian-kohler.path-intellisense
    DavidAnson.vscode-markdownlint
    dbaeumer.vscode-eslint
-   drcallaway.vscode-surround-with
    eamodio.gitlens
    ecmel.vscode-html-css
    eg2.vscode-npm-script
    esbenp.prettier-vscode
-   GitHub.vscode-pull-request-github
+   foxundermoon.shell-format
    GraphQL.vscode-graphql
-   heybourn.headwind
    IronGeek.vscode-env
+   jasonnutter.search-node-modules
    jeremyrajan.vscode-lebab
    meganrogge.template-string-converter
    ms-azuretools.vscode-docker
-   pranaygp.vscode-css-peek
    Prisma.prisma
+   redhat.vscode-xml
    ritwickdey.LiveServer
+   searKing.preview-vscode
    streetsidesoftware.code-spell-checker
+   svelte.svelte-vscode
    tamj0rd2.ts-quickfixes-extension
+   thebanhammer.cfx-lua
    WakaTime.vscode-wakatime
    wix.vscode-import-cost
+   yinfei.luahelper
    yzhang.markdown-all-in-one
    GitHub.github-vscode-theme
    usernamehw.errorlens
    mikestead.dotenv
-   qwtel.sqlite-viewer
-   silvenon.mdx
 )
 
 sudo code --install-extension ${PACKAGES[@]}
